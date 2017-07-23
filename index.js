b3e3ce1c25375bc390b3741ed2d3775be1618229
Steve Peters <sixhops@gmail.com>
2017-07-23 15:10:56 -0700

    Adding the environment variable for port.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
   res.send('Hello world');
 });
 
-app.listen(3000, function() {
+app.listen(process.env.port || 3000, function() {
   console.log('App listening on port 3000...');
 });
