eba10ec255fe8023c7bdc9d435308ce970a371e8
Steve Peters <sixhops@gmail.com>
2017-07-23 15:14:56 -0700

    Changing case of the word PORT

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello world');
+  res.send('Goodbye cruel world');
 });
 
-app.listen(process.env.port || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('App listening on port 3000...');
 });
