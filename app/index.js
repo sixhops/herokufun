4cc6ea3d5fdf4da20b3a5ade8a3b2876f50bcc70
Steve Peters <sixhops@gmail.com>
2017-07-27 13:58:17 -0700

    Reorganizing files

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-  res.send('Goodbye cruel world');
+  res.send('Welcome to the Ancient Bayou!');
 });
 
 app.listen(process.env.PORT || 3000, function() {
